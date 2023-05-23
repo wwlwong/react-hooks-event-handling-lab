dbebd9884c49ff4093fe53036d5ae5257f87ee9d
William Wong <william.wai.lim.wong@gmail.com>
2023-05-23 15:46:46 -0400

    completes react-hooks event handling lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,20 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    function handleChange(event) {
+        console.log(`Entering password...`);
+      }
+    
     return (
-        <div></div>
+        <div>
+            <input
+                type="password"
+                name="password"
+                onChange={handleChange}
+                placeholder="Enter password..."
+            />
+        </div>
     )
 }
 
